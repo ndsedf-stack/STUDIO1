e3d8367f1e91f6f37acacadbedca037c97c31708
ndsedf-stack <ndsedf@gmail.com>
2025-11-05 23:57:06 +0100

    chore(ci): add GH Pages deployment and set Vite base for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/STUDIO1/', // necessary for GitHub Pages
   server: {
     port: 5173
   }
