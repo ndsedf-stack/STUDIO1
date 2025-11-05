f2e612860c5a975bc5d615f93a57d5ed5c651249
ndsedf-stack <ndsedf@gmail.com>
2025-11-06 00:18:17 +0100

    Update comment language in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/STUDIO1/', // necessary for GitHub Pages
+  base: '/STUDIO1/', // nécessaire pour GitHub Pages
   server: {
     port: 5173
   }
